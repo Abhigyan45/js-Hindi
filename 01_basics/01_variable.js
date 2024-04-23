6687e6f423baed84d4399012cdab06cbbe45f38a
Ranjeet Abhigyan <114634780+Abhigyan45@users.noreply.github.com>
2024-04-23 10:10:42 +0000

    variable & dataTypes

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,19 @@
+
+const accountId=23456
+let accountEmail="hij@api.com"
+var accountPassword="12345"
+
+accountCity="Bihar";
+/*
+notes:-
+prefer not use var 
+because of issue in block scope and functional scope 
+*/
+
+//accountId=2;// not allow 
+accountEmail="rock@gamil.com"
+accountPassword="235689"
+accountCity="patna"
+//console.log(accountId);
+
+console.table([accountId,accountEmail,accountPassword,accountCity]);
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,18 @@
+"use strict"; // treat all js code as newer version
+
+ // alert(4+9) // we are using node js, not brower
+
+let isNumber=4563
+
+
+// primitive datatype
+// Number => 2 to the power 53
+// bigInt
+//string => ""
+// boolean => standalone value 
+// undefined =>
+// symbol
+//null
+
+console.log(typeof undefined);
+console.log(typeof null);
